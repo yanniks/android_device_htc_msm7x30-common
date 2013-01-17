8eecf9d4c9ca0348c3ab55619ce12de92f5c2b84
Jay Wu <cn.fyodor@gmail.com>
2013-01-17 13:49:10 +0800

    props: debug.sf.hw is enough for hardware rendering
    Change-Id: I11f1f620e65cab413ded66b0a86109f6481cbb52

--- msm7x30.mk
+++ msm7x30.mk
@@ -132,7 +132,5 @@ PRODUCT_AAPT_PREF_CONFIG := hdpi
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.composition.type=gpu \
-    debug.sf.hw=1 \
-    debug.egl.hw=1
+    debug.sf.hw=1
 
