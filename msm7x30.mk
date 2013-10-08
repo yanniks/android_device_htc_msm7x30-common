cd6c081ef1977675a6661350548e2d292b162b93
Brad <bradthomas127@yahoo.com>
2013-10-08 13:06:46 -0400

    Set bg app limit to half of default

--- msm7x30.mk
+++ msm7x30.mk
@@ -109,4 +109,5 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.composition.type=gpu \
     debug.sf.hw=1 \
-    debug.egl.hw=1
+    debug.egl.hw=1 \
+    ro.sys.fw.bg_apps_limit=12
